d454abf51623947e4681e7b3b1e0a731cf9c450b
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-04 15:17:56 -0600

    Add repr, create_post and json funtions to blog.py

--- blog.py
+++ blog.py
@@ -1,17 +1,39 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, List
+from post import Post
 
 
 class Blog:
-    def __init__(self, title: str, author: str) -> None:
+    def __init__(self, title: Optional[str] = None, author: Optional[str] = None) -> None:
         self.title = title
         self.author = author
         self.posts = []
 
     def __repr__(self) -> str:
-        pass
+        separator = ', '
+        result = 'Blog('
+        param = list()
 
-    def create_post(self, title, content):
-        pass
+        if self.title is not None:
+            param.append(f'title={repr(self.title)}')
+        if self.author is not None:
+            param.append(f'author={repr(self.author)}')
 
-    def json(self):
-        pass
+        result += separator.join(param) + ')'
+        return result
+
+    def create_post(self, title: Optional[str] = None, content: Optional[str] = None) -> None:
+        """Add a new post into the blog lists"""
+        post = Post(title, content)
+        self.posts.append(post)
+
+    def json(self) -> Dict[Optional[str], Optional[str] | Optional[List[Post]]]:
+        """Returns a dict with the attributes."""
+        json = dict()
+        if self.title is not None:
+            json['title'] = self.title
+        if self.author is not None:
+            json['author'] = self.author
+        if len(self.posts) > 0:
+            json['posts'] = [post.json() for post in self.posts]
+
+        return json
