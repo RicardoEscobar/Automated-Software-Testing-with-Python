a0f96cfd594441960356dcb6d8bec8055b797294
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-06 14:58:18 -0600

    Add test/integration/test_blog.py

--- test/integration/test_blog.py
+++ test/integration/test_blog.py
@@ -25,6 +25,29 @@ class BlogTest(unittest.TestCase):
         self.assertEqual(blog.posts[0].title, 'Test post title.')
         self.assertEqual(blog.posts[0].content, 'Test post content.')
 
+    def test_json(self):
+        """Validating the json method."""
+        # Using posts in blog object
+        blog = Blog(title=self.title, author=self.author)
+        blog.create_post('Test post title.', 'Test post content.')
+        expected = {
+            'title': 'Test title.',
+            'author': 'Test author.',
+            'posts': [
+                {'title': 'Test post title.',
+                 'content': 'Test post content.'},
+            ]
+        }
+        self.assertEqual(blog.json(), expected)
+
+        # Test using empty post list
+        blog = Blog(title=self.title, author=self.author)
+        expected = {
+            'title': 'Test title.',
+            'author': 'Test author.',
+        }
+        self.assertEqual(blog.json(), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
