75f125b3e31fd2abf27338ed034d674ca50db399
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-12-20 11:08:56 -0600

    Add test_ask_read_blog and remove unnessesary print test\system\test_app.py

--- test/system/test_app.py
+++ test/system/test_app.py
@@ -25,9 +25,10 @@ class BlogTest(unittest.TestCase):
 
     def test_menu_print_prompt(self):
         """Validates that the menu prompt gets printed out."""
-        with patch('builtins.input') as mocked_input:
-            app.menu()
-            mocked_input.assert_called_with(app.MENU_PROMPT)
+        with patch('builtins.input', return_value='q') as mocked_input:
+            with patch('builtins.print') as mocked_print:
+                app.menu()
+                mocked_input.assert_called_with(app.MENU_PROMPT)
 
     def test_menu_calls_print_blogs(self):
         """Validates that menu function calls the print_blogs function."""
@@ -39,10 +40,20 @@ class BlogTest(unittest.TestCase):
     def test_ask_create_blog(self):
         """Validates ask_create_blog function is called."""
         with patch('builtins.input') as mocked_input:
-            mocked_input.side_effect('Test', 'Test author')
+            mocked_input.side_effect = (self.blog_title, self.blog_author)
             app.ask_create_blog()
 
-            self.assertIsNotNone(app.blogs.get('Test'))
+            self.assertIsNotNone(app.blogs.get(self.blog_title))
+
+    def test_ask_read_blog(self):
+        blog = Blog(title='Test', author='Test author')
+        app.blogs = {'Test': blog}
+        with patch('builtins.input', return_value='Test'):
+            with patch('app.print_posts') as mocked_print_posts:
+                with patch('builtins.print') as mocked_print:
+                    app.ask_read_blog()
+
+                    mocked_print_posts.assert_called_with(blog)
 
 
 if __name__ == '__main__':
